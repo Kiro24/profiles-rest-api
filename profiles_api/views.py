4055be83c3b18f3b6f8ba80d493fb211efcc481c
ti5a <kirollosadel24@yahoo.com>
2020-03-13 20:59:00 +0200

    added hello_api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+
+    def get(self, requerst, format=None):
+        """Returns a list of APIView freatures"""
+
+        users = [
+            'kiro',
+            'mark',
+            'bicha'
+        ]
+
+        return Response({
+            'message': 'hello there!!',
+            'users': users})
