04104d962591562d9d403cdb846389696e1c913c
ti5a <kirollosadel24@yahoo.com>
2020-03-23 19:27:37 +0200

    added login api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from rest_framework import filters
 
 from profiles_api import serializers
@@ -20,7 +22,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     search_fields = ('name', 'email',)
 
 
-
+class UserLoginApiView(ObtainAuthToken):
+    '''handles creating user authentication tokens'''
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 
